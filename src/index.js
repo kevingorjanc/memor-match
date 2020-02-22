2971d1d41dea0d474ac1e926ee5ff2bcf41a5d32
kevin gorjanc <kevin@tutorolearning.com>
2020-02-21 18:46:56 -0800

    new env variables

--- src/index.js
+++ src/index.js
@@ -3,7 +3,9 @@ import ReactDOM from 'react-dom';
 import Backendless from 'backendless';
 import './index.css';
 import StartScreenComponent from "./start-screen-component";
-Backendless.initApp('59FE682A-E700-AB72-FF67-F5C494C10500', 'B1178B46-7137-47DC-88AB-576BBE0500F5');
+
+
+Backendless.initApp(process.env.BACKENDLESS_ID, process.env.BACKENDLESS_JS_PINSET);
 
 
 ReactDOM.render(<StartScreenComponent />, document.getElementById('root'));
